fc0e64a2f53390a0683ac2342ee30cbffef522a6
HamzaThepixelpro <ha444777ha@gmail.com>
2024-06-19 13:48:40 +0500

    End of phase 2... starting Phase 3

---
+++ Ms class phase 2/08_Arrays.js
@@ -0,0 +1,22 @@
+//Arrays
+const myArr = [1, 2, 5, 6, 8, 9, 3];
+const myHeros = new Array("Kahlid", "Ali",  "Abubakr");
+const myArr2 = new Array(1, 2, 5, 6, 8, 9, 3);
+// console.log(myHeros);
+// console.log(myArr[6]);
+// console.log(myArr2[4]);
+
+//Methods in array
+const myArr3 = [1, 2, 5, 6, 8, 9];
+// myArr3.push(4);
+// myArr3.push(9);
+// myArr3.pop();       //removes last element from arry no argument needed to pass
+// console.log(myArr3);
+// myArr3.unshift(0);
+// myArr3.reverse();
+// myArr3.shift();
+console.log(myArr3.includes(1));
+
+let strarr = myArr3.join();
+console.log(strarr);
+console.log(typeof(strarr));
